74a1a5d56fe42c238d4ce5de0ea4ba15b3a5c0f9
Pedro Henrique de Deus Vieira <pedrophdv@outlook.com>
2022-05-17 17:44:09 -0300

    Adicionando Modelo 2 da View e Primeira conexão com Postgres

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import UserView
+from .views import UserView , UserDetail
 
 urlpatterns = [
-    path('', UserView.getUser, name="users"),
-    path('add/', UserView.postUser, name="add")
-
+    path('', UserView.as_view()),
+    path('<int:pk>/', UserDetail.as_view()),
 ]
--- user/views.py
+++ user/views.py
@@ -1,44 +1,43 @@
-from django.shortcuts import render
 from .models import User
 from .serializers import UserSerializer
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+
+from rest_framework import status
+from django.http import Http404
 
 # Create your views here.
 class UserView(APIView):
-    
-    @api_view(['GET'])
-    def getUser(request):
+
+    def get(self , request , format=None):
         users = User.objects.all()
         serializer = UserSerializer(users , many=True)
         return Response(serializer.data)
 
-    @api_view(['GET'])
-    def getDetailUser(request, pk):
-        user = User.objects.get(id=pk)
-        serializer = UserSerializer(user, many=False)
-        return Response(serializer.data)
-
-    @api_view(['POST'])
-    def postUser(request):
+    def post(self , request , format=None):
         serializer = UserSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-        return Response(serializer.data)
+            return Response(serializer.data , status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    @api_view(['PUT'])
-    def updateUser(request , pk):
+class UserDetail(APIView):
+    def get(self, request, pk, format=None):
+        user = User.objects.get(pk=pk)
+        serializer = UserSerializer(user, many=False)
+        return Response(serializer.data)
+    
+    def put(self, request, pk, format=None):
         user = User.objects.get(id=pk)
         serializer = UserSerializer(instance=user, data=request.data)
 
         if serializer.is_valid():
             serializer.save()
         return Response(serializer.data)
-    @api_view(['DELETE'])
-    def deleteUser(request, pk):
+    
+    def delete(self, request, pk, format=None):
         user = User.objects.get(id=pk)
         user.delete()
 

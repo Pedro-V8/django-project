871b78e6cea2e07a7275e669e239c30ff07a6824
Pedro-V8 <pedrophdv@outlook.com>
2022-05-17 23:21:09 -0300

    Modificando models django

--- api/django_postgres/urls.py
+++ api/django_postgres/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from user import urls as user_urls
+from models.user import urls as user_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include(user_urls)),

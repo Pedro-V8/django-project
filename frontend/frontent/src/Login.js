99755cbcebefc594afbfe2503069e81d22ed6cc3
Pedro-V8 <pedrophdv@outlook.com>
2022-06-05 12:09:49 -0300

    Adicionando Frontend e modelo teste de Pagina de Login

---
+++ frontend/frontent/src/Login.js
@@ -0,0 +1,99 @@
+import './App.css';
+import React, { useState } from 'react'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+
+import Typography from '@mui/material/Typography';
+
+
+function Login() {
+
+
+
+
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
+  const [erroStatus, setErroStatus] = useState(false)
+
+  let status = true
+
+  const emailEsenhaExiste = () => {
+    if (email && senha) {
+      status = true
+      setErroStatus(false)
+    } else {
+      status = false
+      setErroStatus(true)
+    }
+  }
+
+  const verifica = () => {
+    emailEsenhaExiste()
+    if (status === true) {
+      if (email.match(/@/) && email.match(/.com/)) {
+        status = true
+        setErroStatus(false)
+      } else {
+        status = false
+        setErroStatus(true)
+      }
+    } else {
+      status = false
+      setErroStatus(true)
+    }
+  }
+
+  const handleSubmit = async () => {
+    verifica()
+    if (status === true) {
+      alert("oi")
+    } else {
+      alert('Dados inválidos, tente novamente')
+    }
+  }
+  return (
+    <>
+      <Grid container className='containerC'>
+        <Grid
+          container
+          justifyContent='center'
+          alignItems='center'
+          direction='column'
+          className='titleContainer'
+        >
+
+          <Typography variant='h4'> Login </Typography>
+          <><h4>   </h4></>
+          <TextField
+            label='Email'
+            error={erroStatus}
+            className='emailText'
+            variant="standard"
+            onChange={(e) => {
+              e.preventDefault()
+              setEmail(e.target.value)
+            }}
+          />
+          <><h1>    </h1></>
+          <TextField
+            label='Senha'
+            error={erroStatus}
+            type='password'
+            className='senhaText'
+            variant="standard"
+            onChange={(e) => {
+              e.preventDefault()
+              setSenha(e.target.value)
+            }}
+          />
+          <><h1>    </h1></>
+          <Button className="botao" onClick={handleSubmit}> ENTRAR </Button>
+
+        </Grid>
+      </Grid>
+    </>
+  );
+}
+
+export default Login;

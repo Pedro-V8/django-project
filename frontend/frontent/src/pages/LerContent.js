261ef242bc240aa55972d9b124b6054d1d40c627
Pedro-V8 <pedrophdv@outlook.com>
2022-07-20 23:30:12 -0300

    Request axios(em andamento)

--- frontend/frontent/src/pages/LerContent.js
+++ frontend/frontent/src/pages/LerContent.js
@@ -1,17 +1,38 @@
 import React, { useContext, useEffect, useState } from 'react'
 import '../pagesCSS/Content.css'
+import axios from 'axios';
 
 
 function LerContent() {
+    const [chapters, setChapters] = useState([])
+
     let caminho = window.location.pathname
     let slug = caminho.substring(5)
-    
-    console.log(slug)
-    return(
+
+
+
+    useEffect(() => {
+        const asyncFunc = async () => {
+            await axios.post('http://127.0.0.1:5000/request_list_chapter', {
+                search_site: slug
+            }).then((res) => {
+                setChapters(res.data)
+            })
+        }
+        asyncFunc()
+
+
+    }, [])
+
+
+
+
+
+    return (
         <>
-    
             <h1>HELLO WORLD</h1>
-    
+            { }
+
         </>
     )
 }
